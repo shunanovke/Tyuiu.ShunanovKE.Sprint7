89fd72548c0916c36a1aa7702ca24205e171d600
shunanovke <shunan05@gmail.com>
2023-12-12 22:05:34 +0500

    12.12

--- Tyuiu.ShunanovKE.Sprint7.Project.V1/FormMain.Designer.cs
+++ Tyuiu.ShunanovKE.Sprint7.Project.V1/FormMain.Designer.cs
@@ -31,21 +31,49 @@ namespace Tyuiu.ShunanovKE.Sprint7.Project.V1
         {
             this.panelUp_SKE = new System.Windows.Forms.Panel();
             this.buttonAbout_SKE = new System.Windows.Forms.Button();
+            this.buttonOpenFile_SKE = new System.Windows.Forms.Button();
+            this.buttonSaveFile_SKE = new System.Windows.Forms.Button();
+            this.panelFile_SKE = new System.Windows.Forms.Panel();
+            this.groupBoxFile_SKE = new System.Windows.Forms.GroupBox();
+            this.dataGridViewFile_SKE = new System.Windows.Forms.DataGridView();
+            this.panelOptions_SKE = new System.Windows.Forms.Panel();
+            this.panelGraph_SKE = new System.Windows.Forms.Panel();
+            this.groupBoxAdd_SKE = new System.Windows.Forms.GroupBox();
+            this.textBoxAddName_SKE = new System.Windows.Forms.TextBox();
+            this.textBoxAddStreet_SKE = new System.Windows.Forms.TextBox();
+            this.textBoxAddOpen_SKE = new System.Windows.Forms.TextBox();
+            this.textBoxAddClose_SKE = new System.Windows.Forms.TextBox();
+            this.textBoxAddPhone_SKE = new System.Windows.Forms.TextBox();
+            this.textBoxAddRate_SKE = new System.Windows.Forms.TextBox();
+            this.labelAddName_SKE = new System.Windows.Forms.Label();
+            this.labelAddStreet_SKE = new System.Windows.Forms.Label();
+            this.labelAddOpen_SKE = new System.Windows.Forms.Label();
+            this.labelAddClose_SKE = new System.Windows.Forms.Label();
+            this.labelAddPhone_SKE = new System.Windows.Forms.Label();
+            this.labelAddRate_SKE = new System.Windows.Forms.Label();
+            this.buttonAdd_SKE = new System.Windows.Forms.Button();
             this.panelUp_SKE.SuspendLayout();
+            this.panelFile_SKE.SuspendLayout();
+            this.groupBoxFile_SKE.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFile_SKE)).BeginInit();
+            this.panelOptions_SKE.SuspendLayout();
+            this.groupBoxAdd_SKE.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelUp_SKE
             // 
+            this.panelUp_SKE.Controls.Add(this.buttonSaveFile_SKE);
+            this.panelUp_SKE.Controls.Add(this.buttonOpenFile_SKE);
             this.panelUp_SKE.Controls.Add(this.buttonAbout_SKE);
             this.panelUp_SKE.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelUp_SKE.Location = new System.Drawing.Point(0, 0);
             this.panelUp_SKE.Name = "panelUp_SKE";
-            this.panelUp_SKE.Size = new System.Drawing.Size(800, 100);
+            this.panelUp_SKE.Size = new System.Drawing.Size(759, 83);
             this.panelUp_SKE.TabIndex = 0;
             // 
             // buttonAbout_SKE
             // 
-            this.buttonAbout_SKE.Location = new System.Drawing.Point(713, 12);
+            this.buttonAbout_SKE.Location = new System.Drawing.Point(672, 12);
             this.buttonAbout_SKE.Name = "buttonAbout_SKE";
             this.buttonAbout_SKE.Size = new System.Drawing.Size(75, 23);
             this.buttonAbout_SKE.TabIndex = 0;
@@ -53,15 +81,216 @@ namespace Tyuiu.ShunanovKE.Sprint7.Project.V1
             this.buttonAbout_SKE.UseVisualStyleBackColor = true;
             this.buttonAbout_SKE.Click += new System.EventHandler(this.buttonAbout_SKE_Click);
             // 
+            // buttonOpenFile_SKE
+            // 
+            this.buttonOpenFile_SKE.Location = new System.Drawing.Point(12, 12);
+            this.buttonOpenFile_SKE.Name = "buttonOpenFile_SKE";
+            this.buttonOpenFile_SKE.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile_SKE.TabIndex = 1;
+            this.buttonOpenFile_SKE.Text = "buttonOpenFile";
+            this.buttonOpenFile_SKE.UseVisualStyleBackColor = true;
+            // 
+            // buttonSaveFile_SKE
+            // 
+            this.buttonSaveFile_SKE.Location = new System.Drawing.Point(93, 12);
+            this.buttonSaveFile_SKE.Name = "buttonSaveFile_SKE";
+            this.buttonSaveFile_SKE.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveFile_SKE.TabIndex = 1;
+            this.buttonSaveFile_SKE.Text = "buttonSaveFile";
+            this.buttonSaveFile_SKE.UseVisualStyleBackColor = true;
+            // 
+            // panelFile_SKE
+            // 
+            this.panelFile_SKE.Controls.Add(this.groupBoxFile_SKE);
+            this.panelFile_SKE.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelFile_SKE.Location = new System.Drawing.Point(0, 475);
+            this.panelFile_SKE.Name = "panelFile_SKE";
+            this.panelFile_SKE.Size = new System.Drawing.Size(759, 146);
+            this.panelFile_SKE.TabIndex = 1;
+            // 
+            // groupBoxFile_SKE
+            // 
+            this.groupBoxFile_SKE.Controls.Add(this.dataGridViewFile_SKE);
+            this.groupBoxFile_SKE.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxFile_SKE.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxFile_SKE.Name = "groupBoxFile_SKE";
+            this.groupBoxFile_SKE.Size = new System.Drawing.Size(759, 146);
+            this.groupBoxFile_SKE.TabIndex = 0;
+            this.groupBoxFile_SKE.TabStop = false;
+            this.groupBoxFile_SKE.Text = "Авторемонтные мастерские";
+            // 
+            // dataGridViewFile_SKE
+            // 
+            this.dataGridViewFile_SKE.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFile_SKE.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewFile_SKE.Location = new System.Drawing.Point(3, 16);
+            this.dataGridViewFile_SKE.Name = "dataGridViewFile_SKE";
+            this.dataGridViewFile_SKE.Size = new System.Drawing.Size(753, 127);
+            this.dataGridViewFile_SKE.TabIndex = 0;
+            // 
+            // panelOptions_SKE
+            // 
+            this.panelOptions_SKE.Controls.Add(this.groupBoxAdd_SKE);
+            this.panelOptions_SKE.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelOptions_SKE.Location = new System.Drawing.Point(0, 83);
+            this.panelOptions_SKE.Name = "panelOptions_SKE";
+            this.panelOptions_SKE.Size = new System.Drawing.Size(467, 392);
+            this.panelOptions_SKE.TabIndex = 2;
+            // 
+            // panelGraph_SKE
+            // 
+            this.panelGraph_SKE.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelGraph_SKE.Location = new System.Drawing.Point(467, 83);
+            this.panelGraph_SKE.Name = "panelGraph_SKE";
+            this.panelGraph_SKE.Size = new System.Drawing.Size(292, 392);
+            this.panelGraph_SKE.TabIndex = 3;
+            // 
+            // groupBoxAdd_SKE
+            // 
+            this.groupBoxAdd_SKE.Controls.Add(this.buttonAdd_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.labelAddStreet_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.labelAddRate_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.labelAddClose_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.labelAddPhone_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.labelAddOpen_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.labelAddName_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.textBoxAddStreet_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.textBoxAddRate_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.textBoxAddClose_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.textBoxAddPhone_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.textBoxAddOpen_SKE);
+            this.groupBoxAdd_SKE.Controls.Add(this.textBoxAddName_SKE);
+            this.groupBoxAdd_SKE.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBoxAdd_SKE.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxAdd_SKE.Name = "groupBoxAdd_SKE";
+            this.groupBoxAdd_SKE.Size = new System.Drawing.Size(467, 100);
+            this.groupBoxAdd_SKE.TabIndex = 0;
+            this.groupBoxAdd_SKE.TabStop = false;
+            this.groupBoxAdd_SKE.Text = "Добавить автомастерскую";
+            // 
+            // textBoxAddName_SKE
+            // 
+            this.textBoxAddName_SKE.Location = new System.Drawing.Point(12, 30);
+            this.textBoxAddName_SKE.Name = "textBoxAddName_SKE";
+            this.textBoxAddName_SKE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddName_SKE.TabIndex = 0;
+            // 
+            // textBoxAddStreet_SKE
+            // 
+            this.textBoxAddStreet_SKE.Location = new System.Drawing.Point(12, 74);
+            this.textBoxAddStreet_SKE.Name = "textBoxAddStreet_SKE";
+            this.textBoxAddStreet_SKE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddStreet_SKE.TabIndex = 0;
+            // 
+            // textBoxAddOpen_SKE
+            // 
+            this.textBoxAddOpen_SKE.Location = new System.Drawing.Point(118, 30);
+            this.textBoxAddOpen_SKE.Name = "textBoxAddOpen_SKE";
+            this.textBoxAddOpen_SKE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddOpen_SKE.TabIndex = 0;
+            // 
+            // textBoxAddClose_SKE
+            // 
+            this.textBoxAddClose_SKE.Location = new System.Drawing.Point(118, 74);
+            this.textBoxAddClose_SKE.Name = "textBoxAddClose_SKE";
+            this.textBoxAddClose_SKE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddClose_SKE.TabIndex = 0;
+            // 
+            // textBoxAddPhone_SKE
+            // 
+            this.textBoxAddPhone_SKE.Location = new System.Drawing.Point(224, 30);
+            this.textBoxAddPhone_SKE.Name = "textBoxAddPhone_SKE";
+            this.textBoxAddPhone_SKE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddPhone_SKE.TabIndex = 0;
+            // 
+            // textBoxAddRate_SKE
+            // 
+            this.textBoxAddRate_SKE.Location = new System.Drawing.Point(224, 74);
+            this.textBoxAddRate_SKE.Name = "textBoxAddRate_SKE";
+            this.textBoxAddRate_SKE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddRate_SKE.TabIndex = 0;
+            // 
+            // labelAddName_SKE
+            // 
+            this.labelAddName_SKE.AutoSize = true;
+            this.labelAddName_SKE.Location = new System.Drawing.Point(12, 14);
+            this.labelAddName_SKE.Name = "labelAddName_SKE";
+            this.labelAddName_SKE.Size = new System.Drawing.Size(57, 13);
+            this.labelAddName_SKE.TabIndex = 1;
+            this.labelAddName_SKE.Text = "Название";
+            // 
+            // labelAddStreet_SKE
+            // 
+            this.labelAddStreet_SKE.AutoSize = true;
+            this.labelAddStreet_SKE.Location = new System.Drawing.Point(12, 58);
+            this.labelAddStreet_SKE.Name = "labelAddStreet_SKE";
+            this.labelAddStreet_SKE.Size = new System.Drawing.Size(38, 13);
+            this.labelAddStreet_SKE.TabIndex = 1;
+            this.labelAddStreet_SKE.Text = "Адрес";
+            // 
+            // labelAddOpen_SKE
+            // 
+            this.labelAddOpen_SKE.AutoSize = true;
+            this.labelAddOpen_SKE.Location = new System.Drawing.Point(115, 14);
+            this.labelAddOpen_SKE.Name = "labelAddOpen_SKE";
+            this.labelAddOpen_SKE.Size = new System.Drawing.Size(91, 13);
+            this.labelAddOpen_SKE.TabIndex = 1;
+            this.labelAddOpen_SKE.Text = "Время открытия";
+            // 
+            // labelAddClose_SKE
+            // 
+            this.labelAddClose_SKE.AutoSize = true;
+            this.labelAddClose_SKE.Location = new System.Drawing.Point(115, 58);
+            this.labelAddClose_SKE.Name = "labelAddClose_SKE";
+            this.labelAddClose_SKE.Size = new System.Drawing.Size(92, 13);
+            this.labelAddClose_SKE.TabIndex = 1;
+            this.labelAddClose_SKE.Text = "Время закрытия";
+            // 
+            // labelAddPhone_SKE
+            // 
+            this.labelAddPhone_SKE.AutoSize = true;
+            this.labelAddPhone_SKE.Location = new System.Drawing.Point(221, 14);
+            this.labelAddPhone_SKE.Name = "labelAddPhone_SKE";
+            this.labelAddPhone_SKE.Size = new System.Drawing.Size(93, 13);
+            this.labelAddPhone_SKE.TabIndex = 1;
+            this.labelAddPhone_SKE.Text = "Номер телефона";
+            // 
+            // labelAddRate_SKE
+            // 
+            this.labelAddRate_SKE.AutoSize = true;
+            this.labelAddRate_SKE.Location = new System.Drawing.Point(221, 58);
+            this.labelAddRate_SKE.Name = "labelAddRate_SKE";
+            this.labelAddRate_SKE.Size = new System.Drawing.Size(48, 13);
+            this.labelAddRate_SKE.TabIndex = 1;
+            this.labelAddRate_SKE.Text = "Рейтинг";
+            // 
+            // buttonAdd_SKE
+            // 
+            this.buttonAdd_SKE.Location = new System.Drawing.Point(362, 71);
+            this.buttonAdd_SKE.Name = "buttonAdd_SKE";
+            this.buttonAdd_SKE.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd_SKE.TabIndex = 2;
+            this.buttonAdd_SKE.Text = "buttonAdd";
+            this.buttonAdd_SKE.UseVisualStyleBackColor = true;
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(759, 621);
+            this.Controls.Add(this.panelGraph_SKE);
+            this.Controls.Add(this.panelOptions_SKE);
+            this.Controls.Add(this.panelFile_SKE);
             this.Controls.Add(this.panelUp_SKE);
             this.Name = "FormMain";
             this.Text = "Form1";
             this.panelUp_SKE.ResumeLayout(false);
+            this.panelFile_SKE.ResumeLayout(false);
+            this.groupBoxFile_SKE.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFile_SKE)).EndInit();
+            this.panelOptions_SKE.ResumeLayout(false);
+            this.groupBoxAdd_SKE.ResumeLayout(false);
+            this.groupBoxAdd_SKE.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +299,27 @@ namespace Tyuiu.ShunanovKE.Sprint7.Project.V1
 
         private System.Windows.Forms.Panel panelUp_SKE;
         private System.Windows.Forms.Button buttonAbout_SKE;
+        private System.Windows.Forms.Button buttonSaveFile_SKE;
+        private System.Windows.Forms.Button buttonOpenFile_SKE;
+        private System.Windows.Forms.Panel panelFile_SKE;
+        private System.Windows.Forms.GroupBox groupBoxFile_SKE;
+        private System.Windows.Forms.DataGridView dataGridViewFile_SKE;
+        private System.Windows.Forms.Panel panelOptions_SKE;
+        private System.Windows.Forms.GroupBox groupBoxAdd_SKE;
+        private System.Windows.Forms.Label labelAddStreet_SKE;
+        private System.Windows.Forms.Label labelAddRate_SKE;
+        private System.Windows.Forms.Label labelAddClose_SKE;
+        private System.Windows.Forms.Label labelAddPhone_SKE;
+        private System.Windows.Forms.Label labelAddOpen_SKE;
+        private System.Windows.Forms.Label labelAddName_SKE;
+        private System.Windows.Forms.TextBox textBoxAddStreet_SKE;
+        private System.Windows.Forms.TextBox textBoxAddRate_SKE;
+        private System.Windows.Forms.TextBox textBoxAddClose_SKE;
+        private System.Windows.Forms.TextBox textBoxAddPhone_SKE;
+        private System.Windows.Forms.TextBox textBoxAddOpen_SKE;
+        private System.Windows.Forms.TextBox textBoxAddName_SKE;
+        private System.Windows.Forms.Panel panelGraph_SKE;
+        private System.Windows.Forms.Button buttonAdd_SKE;
     }
 }
 
